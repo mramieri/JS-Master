7960591750e121bf3771e53e4aa7b6199181c0f1
amarula <maru.ramieri@gmail.com>
2022-05-26 12:59:55 -0300

    16. proyecto email OK

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,135 +1,132 @@
-// 1. Variables
+//Variables
 const btnEnviar = document.querySelector("#enviar");
-const btnReset = document.querySelector('#resetBtn');
-const formulario = document.querySelector("#enviar-mail");
+const btnReset = document.querySelector("#resetBtn");
+const formulario =  document.querySelector("#enviar-mail");
 
-// 4. Variables para campos
+// Variables de campos
 const email = document.querySelector("#email");
 const asunto = document.querySelector("#asunto");
 const mensaje = document.querySelector("#mensaje");
-const er =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            
 
-// 3. EventListeners
+//Eventos
+eventListeners();
+function eventListeners(){
 
-EventListeners();
-function EventListeners() {
-  //Cuando la app arranca
+  //Cuando la app arranca:
   document.addEventListener("DOMContentLoaded", iniciarApp);
 
-  //4. Campos del formulario
+  //Campos del formulario
   email.addEventListener("blur", validarFormulario);
   asunto.addEventListener("blur", validarFormulario);
   mensaje.addEventListener("blur", validarFormulario);
 
-  // Reinicia el formulario
-    btnReset.addEventListener('click', resetearFormulario);
+  //Enviar email
+  formulario.addEventListener("submit", enviarEmail);
+  
+  //Reinicia el formulario
+  btnReset.addEventListener("click", resetearFormulario);
 
-  // Enviar email
-  formulario.addEventListener('submit', enviarEmail);
 }
 
-// 2. Funciones
-
-function iniciarApp() {
+//Funciones
+function iniciarApp(){
   btnEnviar.disabled = true;
-  btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+  btnEnviar.classList.add("cursor-not-allowed", "opacity-50");
 }
 
-//5. Valida el formulario
+
+//Valida el formulario (Para, Asunto y msj)
 function validarFormulario(e) {
-  if (e.target.value.length > 0) {
-    //Elimina los errores...
-    const error = document.querySelector("p.error");
-    if (error) {
-      error.remove();
-    }
+  
+      //Validación de campos vacios o no
+    if (e.target.value.length > 0) {
 
-    e.target.classList.remove("border", "border-red-500"); //Siempre es buena idea eliminar las clases que ya no estoy utilizando
-    e.target.classList.add("border", "border-green-500");
-  } else {
-    e.target.classList.remove("border", "border-green-500");
-    e.target.classList.add("border", "border-red-500");
-    mostrarError("Todos los campos son obligatorios");
-  }
+           //Elimina los errores..
+            const error = document.querySelector("p.error");
+            if(error){
+              error.remove();
+            }
+            
 
-  //asi accedo a lo que el usuario completo en el input
+      e.target.classList.remove("border","border-red-500");
+      e.target.classList.add("border","border-green-500");
 
-  if (e.target.type === "email") {         
-    if ( er.test(e.target.value)) {
-         //Elimina los errores...
-      const error = document.querySelector("p.error");
-      if (error) {
-        error.remove();
+      } else {
+        e.target.classList.remove("border","border-green-500");
+        e.target.classList.add("border","border-red-500");
+        mostrarError("Todos los campos son obligatorios");
       }
+        //Validación del e@mail en Para:
+        if(e.target.type === "email"){
+              
+            if (er.test( e.target.value )){
+                  //Elimina los errores..
+                const error = document.querySelector("p.error");
+                   if(error){
+                  error.remove();
+                   }
+
+        e.target.classList.remove("border","border-red-500");
+        e.target.classList.add("border","border-green-500");
+        
+            } else {
+        e.target.classList.remove("border","border-green-500");
+        e.target.classList.add("border","border-red-500");
+                  mostrarError("E-mail invalido, intenta nuevamente");
+            }
+          }      
     
-    e.target.classList.remove("border", "border-red-500"); //Siempre es buena idea eliminar las clases que ya no estoy utilizando
-    e.target.classList.add("border", "border-green-500");
-  } else {
-    e.target.classList.remove("border", "border-green-500");
-    e.target.classList.add("border", "border-red-500");
-    mostrarError("Email no valido");
-  }
+            if (er.test( email.value ) && asunto.value !== "" && mensaje.value !== "") {
+              btnEnviar.disabled = false;
+              btnEnviar.classList.remove("cursor-not-allowed", "opacity-50");
+            } 
     }
 
-    if (er.test(email.value) && asunto.value !== '' && mensaje.value !== '') {
-        btnEnviar.disabled = false;
-         btnEnviar.classList.remove("cursor-not-allowed", "opacity-50");      
-    } 
-}
 
-function mostrarError(mensaje) {
-  const mensajeError = document.createElement("p");
-  mensajeError.textContent = mensaje;
-  mensajeError.classList.add(
-    "border",
-    "border-red-500",
-    "background-red-100",
-    "text-red-500",
-    "p-3",
-    "mt-5",
-    "text-center",
-    "error"
-  );
+    function mostrarError(mensaje) {
+      const mensajeError = document.createElement("p");
+      mensajeError.textContent = mensaje;
+      mensajeError.classList.add("border","border-red-500", "background-red-100", "text-red-500", "p-3", "mt-5", "text-center", "error" );
 
-  const errores = document.querySelectorAll(".error"); // Reminder: .lenght solo existe en querySelectoraALL
-  if (errores.length === 0) {
-    formulario.appendChild(mensajeError);
-  }
+        const errores = document.querySelectorAll(".error"); //si ya hay un mensaje previo, no me agregues más avisos
+          if (errores.length === 0) {
+            formulario.appendChild(mensajeError);
+          } 
+    
+    }
 
-}
+//Envia el email
+      function enviarEmail(e){
+        e.preventDefault();
 
-// Envia el email 
-function enviarEmail(e) {
-    e.preventDefault();
+        //Mostrar el spinner
+        const spinner = document.querySelector("#spinner");
+        spinner.style.display = "flex";
 
-   //Mostrar el spinner
-   const spinner = document.querySelector('#spinner');
-   spinner.style.display = 'flex';
+          //Dsp de 3'', ocultar el spinner y mostrar un msjito:
+          setTimeout(() => {
+              spinner.style.display = "none";
 
-   // Despues de 3 segundos ocultar el spinner y mostrar el mensaje:
-    setTimeout(() => {
-        spinner.style.display = 'none';
+              //Msj "envio de exito"
+              const parrafo = document.createElement("p");
+              parrafo.textContent = "El mensaje ha sido enviado de manera exitooosaa";
+              parrafo.classList.add("text-center", "my-10", "p-2", "bg-pink-100", "text-black", "font-bold", "uppercase");
+              
+              //Inserta el parrafo antes del spinner
+              formulario.insertBefore(parrafo, spinner)
 
-          // Mensaje que me dice que se envio correctamente
-          const parrafo = document.createElement('p');
-          parrafo.textContent = 'El mensaje se envio correctamente'
-          parrafo.classList.add('text-center', 'my-10', 'p-2', 'bg-green-500', 'text-white', 'font-bold', 'uppercase');
+              setTimeout(() => {
+                parrafo.remove(); // Elimina msj de exito
+                resetearFormulario(); //dsp de 5''
+              }, 5000);
 
-          // Inserta el parrafo antes del spinner
-          formulario.insertBefore(parrafo, spinner);
-       setTimeout(() => {
-           parrafo.remove(); // Eliminar el msj de exito
+          }, 3000);
+ }
 
-           resetearFormulario();
-           }, 5000 );
-       }, 3000);          
-
-}
-
-//Funcion que resetea el formulario
-function resetearFormulario() {
-    formulario.reset();
-
-    iniciarApp();
-}
+ //Funcion que resetea el formi
+ function resetearFormulario() {
+  formulario.reset();
+  iniciarApp();
+ }
